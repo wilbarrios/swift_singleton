de81fefa8858881d03171d3fdf7167f0c6d289d7
Wilmer Barrios <wilmer@grainchain.io>
2021-02-20 15:51:43 -0600

    Project initialization as MacOS Framework with tests

---
+++ Singleton/SingletonTests/SingletonTests.swift
@@ -0,0 +1,16 @@
+//
+//  SingletonTests.swift
+//  SingletonTests
+//
+//  Created by Wilmer Barrios on 20/02/21.
+//
+
+import XCTest
+@testable import Singleton
+
+class SingletonTests: XCTestCase {
+
+    func test_init() {
+//        XCTFail()
+    }
+}
