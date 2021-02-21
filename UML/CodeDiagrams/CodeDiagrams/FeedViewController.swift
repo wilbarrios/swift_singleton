1b20a0a34e7bd0ad37631133be17e9f4a6116db6
Wilmer Barrios <wilmer@grainchain.io>
2021-02-21 15:03:02 -0600

    `FeedViewController` `FeedLoader` definition and implementation

--- UML/CodeDiagrams/CodeDiagrams/FeedViewController.swift
+++ UML/CodeDiagrams/CodeDiagrams/FeedViewController.swift
@@ -8,4 +8,30 @@
 import Foundation
 import UIKit
 
-class FeedViewController: UIViewController {}
+typealias FeedLoader = (([String]) -> Void) -> Void
+
+class FeedViewController: UIViewController {
+    // MARK: Dependencies
+    var loadFeed: FeedLoader!
+    
+    // MARK: Datasource properties
+    var items = [String]()
+    
+    // MARK: Inits
+    convenience init(loadFeed: @escaping FeedLoader) {
+        self.init()
+        self.loadFeed = loadFeed
+    }
+    
+    // MARK: VC Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadFeed { items = $0; updateUI() } // Does not need a `self` explicit declaration.
+    }
+    
+    // MARK: Helpers
+    private func updateUI() {
+        print("UI Updated with items: \(debugPrint(items))")
+    }
+}
+
