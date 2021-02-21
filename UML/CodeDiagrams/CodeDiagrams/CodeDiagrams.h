96ba0b64880172dd1988918c357845806793388d
Wilmer Barrios <wilmer@grainchain.io>
2021-02-20 19:18:44 -0600

    UML/CodeDiagrams project and readme file added

---
+++ UML/CodeDiagrams/CodeDiagrams/CodeDiagrams.h
@@ -0,0 +1,18 @@
+//
+//  CodeDiagrams.h
+//  CodeDiagrams
+//
+//  Created by Wilmer Barrios on 20/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CodeDiagrams.
+FOUNDATION_EXPORT double CodeDiagramsVersionNumber;
+
+//! Project version string for CodeDiagrams.
+FOUNDATION_EXPORT const unsigned char CodeDiagramsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodeDiagrams/PublicHeader.h>
+
+
---
+++ UML/README.md
@@ -0,0 +1,3 @@
+# Code basic diagrams, UML
+
+Communication tool for ourselves and other developers in the team, and documentation, and makes easy to see dependency between modules and visual identification of potential memory leaks.
