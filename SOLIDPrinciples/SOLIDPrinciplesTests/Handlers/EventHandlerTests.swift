7a64678abf2bcab10de5bc1104a0a92381750914
Wilmer Barrios <wilmer@grainchain.io>
2021-02-28 19:28:56 -0600

    `EventHandler` retreives error on Event Service error

--- SOLIDPrinciples/SOLIDPrinciplesTests/Handlers/EventHandlerTests.swift
+++ SOLIDPrinciples/SOLIDPrinciplesTests/Handlers/EventHandlerTests.swift
@@ -42,9 +42,35 @@ class EventHandlerTests: XCTestCase {
     func test_sendEventTriggersEventNotificationOnEventService() {
         let (sut, service) = makeSUT()
         
-        sut.sendEvent(EventStub.anyEvent)
+        sut.sendEvent(makeAnyEvent())
         
         XCTAssertEqual(service.triggeredActions, [.send])
+    }
+    
+    func test_retreivesErrorOnServiceError() {
+        let (sut, service) = makeSUT()
+        let exp = expectation(description: "Send event service was not completed.")
+        
+        let error = makeAnyError()
+        var retrivedError: Error?
+        sut.sendEvent(makeAnyEvent()) { (error) in
+            retrivedError = error
+            exp.fulfill()
+        }
+        service.complete(withError: error)
+        
+        wait(for: [exp], timeout: 0.1)
+        XCTAssertNotNil(retrivedError, "An error was not retrived on Event notification service error.")
+        XCTAssertEqual(error, retrivedError! as NSError)
+    }
+    
+    // MARK: Helpers
+    private func makeAnyError() -> NSError {
+        return NSError(domain: "Any error", code: 0, userInfo: nil)
+    }
+    
+    private func makeAnyEvent() -> Event {
+        EventStub.anyEvent
     }
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: EventHandler, service: EventServiceMock) {
@@ -56,14 +82,23 @@ class EventHandlerTests: XCTestCase {
     }
     
     class EventServiceMock: EventService {
+        typealias EventCompletionHandler = ((Error?) -> Void)
+        
         enum Action {
             case send
         }
         
         var triggeredActions = [Action]()
+        private var completions = [EventCompletionHandler]()
         
-        func send(event: String, completion: @escaping (Error?) -> Void) {
+        
+        func send(event: String, completion: @escaping EventCompletionHandler) {
             triggeredActions.append(.send)
+            completions.append(completion)
+        }
+        
+        func complete(withError error: Error?, index: Int = 0) {
+            completions[index](error)
         }
     }
 }
