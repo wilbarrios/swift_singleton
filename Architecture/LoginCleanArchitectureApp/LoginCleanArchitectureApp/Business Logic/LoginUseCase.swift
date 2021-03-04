4dd98d4b0013f0d57f572b87ade83722f4909c9b
Wilmer Barrios <wilmer@grainchain.io>
2021-03-03 19:43:59 -0600

    `LoginUseCaseController` login succeed delivers login success

--- Architecture/LoginCleanArchitectureApp/LoginCleanArchitectureApp/Business Logic/LoginUseCase.swift
+++ Architecture/LoginCleanArchitectureApp/LoginCleanArchitectureApp/Business Logic/LoginUseCase.swift
@@ -8,10 +8,16 @@
 import Foundation
 
 protocol LoginUseCase {
+    func login(username: String, password: String)
+}
+
+protocol LoginService {
     func login(username: String, password: String, completion: @escaping (Error?) -> Void)
 }
 
-struct User {}
+struct User {
+    let userName: String
+}
 
 protocol LoginUseCaseOutput {
     func loginSuceeded(user: User)
@@ -21,15 +27,22 @@ protocol LoginUseCaseOutput {
 final class LoginUseCaseController: LoginUseCase {
     
     private let output: LoginUseCaseOutput
-    private let loginProvider: LoginUseCase
+    private let loginService: LoginService
     
-    init(output: LoginUseCaseOutput, loginProvider: LoginUseCase) {
+    init(output: LoginUseCaseOutput, loginService: LoginService) {
         self.output = output
-        self.loginProvider = loginProvider
+        self.loginService = loginService
     }
     
-    func login(username: String, password: String, completion: @escaping (Error?) -> Void) {
-        loginProvider.login(username: username, password: password, completion: completion)
+    func login(username: String, password: String) {
+        let user = map(userName: username)
+        loginService.login(username: username, password: password) { [weak self] _ in
+            self?.output.loginSuceeded(user: user)
+        }
+    }
+    
+    private func map(userName: String) -> User {
+        return User(userName: userName)
     }
     
 }
--- Architecture/LoginCleanArchitectureApp/LoginCleanArchitectureAppTests/LoginUseCaseController.swift
+++ Architecture/LoginCleanArchitectureApp/LoginCleanArchitectureAppTests/LoginUseCaseController.swift
@@ -17,42 +17,73 @@ class LoginUseCaseControllerTests: XCTestCase {
         XCTAssertEqual(provider.triggeredActions, [])
     }
     
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LoginUseCaseController, output: LoginUseCaseOutputMock, provider: LoginProviderMock) {
-        let p = LoginProviderMock()
+    func test_loginSucceed_deliversLoginSucces() {
+        let (sut, out, provider) = makeSUT()
+        
+        sut.login(username: makeAnyUser(), password: makeAnyPassword())
+        provider.complete(nil)
+        
+        XCTAssertEqual(provider.triggeredActions, [.login])
+        XCTAssertEqual(out.triggeredActions, [.loginSuceeded])
+    }
+    
+    // MARK: Helpers
+    
+    private func makeAnyUser() -> String {
+        return "anyUser"
+    }
+    
+    private func makeAnyPassword() -> String {
+        return "anyPassword"
+    }
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LoginUseCaseController, output: LoginUseCaseOutputMock, service: LoginServiceMock) {
+        let p = LoginServiceMock()
         let out = LoginUseCaseOutputMock()
-        let sut = LoginUseCaseController(output: out, loginProvider: p)
+        let sut = LoginUseCaseController(output: out, loginService: p)
         trackMemoryLeaks(p, file: file, line: line)
         trackMemoryLeaks(out, file: file, line: line)
         trackMemoryLeaks(sut, file: file, line: line)
         return (sut, out, p)
     }
     
+    // MARK: Testing entities
+    
     class LoginUseCaseOutputMock: LoginUseCaseOutput {
         enum Action: Equatable {
-            
+            case loginSuceeded
+            , loginFailed
         }
         var triggeredActions = [Action]()
         
         func loginSuceeded(user: User) {
-            
+            triggeredActions.append(.loginSuceeded)
         }
         
         func loginFailed(error: Error) {
-            
+            triggeredActions.append(.loginFailed)
         }
     }
     
-    class LoginProviderMock: LoginUseCase {
+    class LoginServiceMock: LoginService {
         
+        typealias LoginResult = Error?
+        private typealias LoginCompletionHandler = (LoginResult) -> Void
         
         enum Action: Equatable {
-            
+            case login
         }
         
         var triggeredActions = [Action]()
+        private var completions = [LoginCompletionHandler]()
         
         func login(username: String, password: String, completion: @escaping (Error?) -> Void) {
-            
+            triggeredActions.append(.login)
+            completions.append(completion)
+        }
+        
+        func complete(_ result: LoginResult, index: Int = 0) {
+            completions[index](result)
         }
     }
 }
