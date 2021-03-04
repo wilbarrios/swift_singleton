7e54f42e6e403f894d29bb259deae8490817cd86
Wilmer Barrios <wilmer@grainchain.io>
2021-03-03 18:58:35 -0600

    `LoginCleanArchitectureApp` project initialization

---
+++ Architecture/LoginCleanArchitectureApp/LoginCleanArchitectureApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginCleanArchitectureApp
+//
+//  Created by Wilmer Barrios on 03/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Architecture/LoginCleanArchitectureApp/LoginCleanArchitectureAppTests/LoginCleanArchitectureAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginCleanArchitectureAppTests.swift
+//  LoginCleanArchitectureAppTests
+//
+//  Created by Wilmer Barrios on 03/03/21.
+//
+
+import XCTest
+
+class LoginCleanArchitectureAppTests: XCTestCase {
+
+    func test_fail() {
+//        XCTFail()
+        
+    }
+
+}
