83757b3144bac47b35790a09ea06254ef21a7dcb
Wilmer Barrios <wilmerbt@yahoo.com>
2021-03-09 20:21:41 -0600

    `AutomatedTestMacOSFramework ` project initialized

---
+++ Automated Testing/AutomatedTestMacOSFramework/AutomatedTestMacOSFramework/AutomatedTestMacOSFramework.h
@@ -0,0 +1,18 @@
+//
+//  AutomatedTestMacOSFramework.h
+//  AutomatedTestMacOSFramework
+//
+//  Created by Wilmer Barrios on 09/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AutomatedTestMacOSFramework.
+FOUNDATION_EXPORT double AutomatedTestMacOSFrameworkVersionNumber;
+
+//! Project version string for AutomatedTestMacOSFramework.
+FOUNDATION_EXPORT const unsigned char AutomatedTestMacOSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutomatedTestMacOSFramework/PublicHeader.h>
+
+
