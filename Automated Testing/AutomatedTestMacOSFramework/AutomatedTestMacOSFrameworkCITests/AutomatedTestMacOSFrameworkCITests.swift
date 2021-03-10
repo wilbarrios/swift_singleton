0346570bffdc8d6771d43f0d990740edf5cc7d12
Wilmer Barrios <wilmerbt@yahoo.com>
2021-03-09 20:27:20 -0600

    `CI` scheme and `AutomatedTestMacOSFrameworkTests` target added with configuration

---
+++ Automated Testing/AutomatedTestMacOSFramework/AutomatedTestMacOSFrameworkCITests/AutomatedTestMacOSFrameworkCITests.swift
@@ -0,0 +1,16 @@
+//
+//  AutomatedTestMacOSFrameworkCITests.swift
+//  AutomatedTestMacOSFrameworkCITests
+//
+//  Created by Wilmer Barrios on 09/03/21.
+//
+
+import XCTest
+
+class AutomatedTestMacOSFrameworkCITests: XCTestCase {
+
+    func test_init() {
+//        XCTFail()
+    }
+
+}
---
+++ Automated Testing/AutomatedTestMacOSFramework/AutomatedTestMacOSFrameworkTests/AutomatedTestMacOSFrameworkTests.swift
@@ -0,0 +1,15 @@
+//
+//  AutomatedTestMacOSFrameworkTests.swift
+//  AutomatedTestMacOSFrameworkTests
+//
+//  Created by Wilmer Barrios on 09/03/21.
+//
+
+import XCTest
+
+class AutomatedTestMacOSFrameworkTests: XCTestCase {
+
+    func test_init() {
+        
+    }
+}
