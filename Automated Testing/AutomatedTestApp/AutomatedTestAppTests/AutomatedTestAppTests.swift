32bd98f2fd72bb48838ab836f792ddcd2549afe1
Wilmer Barrios <wilmerbt@yahoo.com>
2021-03-09 19:45:44 -0600

    Main test target added

---
+++ Automated Testing/AutomatedTestApp/AutomatedTestAppTests/AutomatedTestAppTests.swift
@@ -0,0 +1,14 @@
+//
+//  AutomatedTestAppTests.swift
+//  AutomatedTestAppTests
+//
+//  Created by Wilmer Barrios on 09/03/21.
+//
+
+import XCTest
+
+class AutomatedTestAppTests: XCTestCase {
+    func test_init() {
+//        XCTFail()
+    }
+}
