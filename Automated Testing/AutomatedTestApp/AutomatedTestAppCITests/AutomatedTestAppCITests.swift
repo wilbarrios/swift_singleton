441b3ed6e57ba345accbcba95af4fb26fb36ed8a
Wilmer Barrios <wilmerbt@yahoo.com>
2021-03-09 20:14:58 -0600

    `CI` scheme and `AutomatedTestAppCITests` target added with configurations

---
+++ Automated Testing/AutomatedTestApp/AutomatedTestAppCITests/AutomatedTestAppCITests.swift
@@ -0,0 +1,16 @@
+//
+//  AutomatedTestAppCITests.swift
+//  AutomatedTestAppCITests
+//
+//  Created by Wilmer Barrios on 09/03/21.
+//
+
+import XCTest
+
+class AutomatedTestAppCITests: XCTestCase {
+
+    func test_init() {
+        
+    }
+
+}
