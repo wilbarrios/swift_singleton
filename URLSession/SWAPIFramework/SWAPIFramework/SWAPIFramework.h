f669b36cbe8631a4f3c3f506d2ba74d84afaca98
Wilmer Barrios <wilmerbt@yahoo.com>
2021-03-11 19:37:23 -0600

    `SWAPIFramework` initialization

---
+++ URLSession/SWAPIFramework/SWAPIFramework/SWAPIFramework.h
@@ -0,0 +1,18 @@
+//
+//  SWAPIFramework.h
+//  SWAPIFramework
+//
+//  Created by Wilmer Barrios on 11/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SWAPIFramework.
+FOUNDATION_EXPORT double SWAPIFrameworkVersionNumber;
+
+//! Project version string for SWAPIFramework.
+FOUNDATION_EXPORT const unsigned char SWAPIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWAPIFramework/PublicHeader.h>
+
+
