d18a944e0e0c0569f397f89d9660c67e9b14a4d0
Wilmer Barrios <wilmerbt@yahoo.com>
2021-03-11 19:39:56 -0600

    `SWAPIFramework` tests target added

---
+++ URLSession/SWAPIFramework/SWAPIFrameworkTests/SWAPIFrameworkTests.swift
@@ -0,0 +1,16 @@
+//
+//  SWAPIFrameworkTests.swift
+//  SWAPIFrameworkTests
+//
+//  Created by Wilmer Barrios on 11/03/21.
+//
+
+import XCTest
+
+class SWAPIFrameworkTests: XCTestCase {
+
+    func test_init() {
+//        XCTFail()
+    }
+
+}
