e2c57376304513413441d0a28fbb0772926efc41
Wilmer Barrios <wilmerbt@yahoo.com>
2021-03-11 20:00:10 -0600

    `RemotePeopleFeed` does not request data from url on initalization.

--- URLSession/README.md
+++ URLSession/README.md
@@ -1,3 +1,9 @@
 # URLSession
 
+Project destinated to make tests with URLSession framework to improve skills and have more robust frameworks for HTTP requests.
+
+## SWAPI
+
+First feature that I want to improve is a Star Wars people related feed client.
+
 
---
+++ URLSession/SWAPIFramework/SWAPIFrameworkTests/RemotePeopleFeedTests.swift
@@ -0,0 +1,50 @@
+//
+//  RemotePeopleFeedTests.swift
+//  SWAPIFrameworkTests
+//
+//  Created by Wilmer Barrios on 11/03/21.
+//
+
+import Foundation
+import XCTest
+
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class RemotePeopleFeed {
+    // MARK: Properties
+    let client: HTTPClient
+    
+    // MARK: Inits
+    init(client: HTTPClient) {
+        self.client = client
+    }
+}
+
+class RemotePeopleFeedTests: XCTestCase {
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = makeSUT()
+        
+        XCTAssertNil(client.requestedURL, "Initialization requests data from URL")
+    }
+    
+    // MARK: Helpers
+    
+    private func makeSUT() -> (sut: RemotePeopleFeed, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemotePeopleFeed(client: client)
+        
+        return (sut, client)
+    }
+    
+    // MARK: Testing entities
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            
+        }
+    }
+}
