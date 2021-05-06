0a48c4be92fc521a7bc131bd5e207fe068f0f145
Wilmer Barrios <wilmerbt@yahoo.com>
2021-05-05 20:21:13 -0500

    `FeedController` init does not fetch feed

--- MainDispatchQueueDecorator/MainDispatchQueueDecorator/MainDispatchQueueDecoratoriOSTests/FeedControllerTests.swift
+++ MainDispatchQueueDecorator/MainDispatchQueueDecorator/MainDispatchQueueDecoratoriOSTests/FeedControllerTests.swift
@@ -4,3 +4,33 @@
 //
 //  Created by Wilmer Barrios on 05/05/21.
 //
+
+import UIKit
+import XCTest
+import MainDispatchQueueDecorator
+
+final class FeedController: UITableViewController {
+    private var loader: FeedLoader?
+    
+    convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+}
+
+class FeedControllerTests: XCTestCase {
+    func test_init_doesNotFetchFeed() {
+        let loader = FeedLoaderMock()
+        let _ = FeedController(loader: loader)
+        
+        XCTAssertEqual(loader.loadRequestsCount, 0)
+    }
+    
+    private class FeedLoaderMock: FeedLoader {
+        var loadRequestsCount: Int = 0
+        
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            loadRequestsCount += 1
+        }
+    }
+}
