578ab5f438a458459f3c24003bbdaf8159770a05
Wilmer Barrios <wilmerbt@yahoo.com>
2021-05-10 19:50:14 -0500

    `ProgrammaticController` view did load load label with initialized text

--- SnapshotTesting/SnapshotTest/SnapshotTestTests/ProgrammaticControllerTests.swift
+++ SnapshotTesting/SnapshotTest/SnapshotTestTests/ProgrammaticControllerTests.swift
@@ -5,3 +5,15 @@
 //  Created by Wilmer Barrios on 10/05/21.
 //
 
+import XCTest
+@testable import SnapshotTest
+
+class ProgrammaticControllerTests: XCTestCase {
+    func test_viewDidLoad_loadLabelWithInitializedText() {
+        let expectedText = "My Text"
+        let sut = ProgrammaticController(text: expectedText)
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(expectedText, sut.label.text)
+    }
+}
