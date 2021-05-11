f73ba7dc2c70bc427b1a137a49f712bbd8cc22e4
Wilmer Barrios <wilmerbt@yahoo.com>
2021-05-10 20:18:37 -0500

    `record` helper function added to record all snapshots, `EMPTY_CONTROLLER.png` file added

--- SnapshotTesting/SnapshotTest/SnapshotTestCITests/ProgrammaticControllerSnapshotTests.swift
+++ SnapshotTesting/SnapshotTest/SnapshotTestCITests/ProgrammaticControllerSnapshotTests.swift
@@ -14,13 +14,32 @@ class ProgrammaticControllerSnapshotTests: XCTestCase {
     func test_emptyText() {
         let sut = makeSUT()
         
-        let snapshot = sut.snapshot()
-        
+        record(snapshot: sut.snapshot(), name: "EMPTY_CONTROLLER")
     }
     
     // MARK: Helpers
     private func makeSUT(text: String? = nil) -> ProgrammaticController {
         return ProgrammaticController(text: text ?? "")
+    }
+    
+    private func record(snapshot: UIImage, name: String, file: StaticString = #file, line: UInt = #line) {
+        guard let snapshotData = snapshot.pngData() else {
+            XCTFail("Failed to generate PNG data representation from snapshot", file: file, line: line)
+            return
+        }
+        
+        // ../SnapshotTestCITests/snapshots/[name].png
+        let snapshotURL = URL(fileURLWithPath: String(describing: file))
+            .deletingLastPathComponent()
+            .appendingPathComponent("snapshots")
+            .appendingPathComponent("\(name).png")
+        
+        do {
+            try FileManager.default.createDirectory(at: snapshotURL.deletingLastPathComponent(), withIntermediateDirectories: true)
+            try snapshotData.write(to: snapshotURL)
+        } catch {
+            XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
+        }
     }
 }
 
