8fada54db62d19f714a56e07b0ec95cd9f613116
Wilmer Barrios <wilmerbt@yahoo.com>
2021-05-12 18:32:51 -0500

    `ScrollApp` added

---
+++ SnapshotTesting/ScrollApp/ScrollApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollApp
+//
+//  Created by Wilmer Barrios on 12/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
