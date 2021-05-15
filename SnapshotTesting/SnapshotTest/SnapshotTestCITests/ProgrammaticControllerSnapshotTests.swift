7ce10d98cbb53215ea92291aa005b98426340d2b
Wilmer Barrios <wilmerbt@yahoo.com>
2021-05-15 17:58:41 -0500

    Assertion function added to snapshot tests

--- SnapshotTesting/SnapshotTest/SnapshotTestCITests/ProgrammaticControllerSnapshotTests.swift
+++ SnapshotTesting/SnapshotTest/SnapshotTestCITests/ProgrammaticControllerSnapshotTests.swift
@@ -14,13 +14,13 @@ class ProgrammaticControllerSnapshotTests: XCTestCase {
     func test_emptyText() {
         let sut = makeSUT()
         
-        record(snapshot: sut.snapshot(), name: "EMPTY_CONTROLLER")
+        assert(snapshot: sut.snapshot(), name: "EMPTY_CONTROLLER")
     }
     
     func test_shortText() {
         let sut = makeSUT(text: "Short text")
         
-        record(snapshot: sut.snapshot(), name: "SHORT_TEXT_CONTROLLER")
+        assert(snapshot: sut.snapshot(), name: "SHORT_TEXT_CONTROLLER")
     }
     
     // MARK: Helpers
@@ -39,6 +39,40 @@ class ProgrammaticControllerSnapshotTests: XCTestCase {
             .deletingLastPathComponent()
             .appendingPathComponent("snapshots")
             .appendingPathComponent("\(name).png")
+        
+        do {
+            try FileManager.default.createDirectory(at: snapshotURL.deletingLastPathComponent(), withIntermediateDirectories: true)
+            try snapshotData.write(to: snapshotURL)
+        } catch {
+            XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
+        }
+    }
+    
+    private func assert(snapshot: UIImage, name: String, file: StaticString = #file, line: UInt = #line) {
+        guard let snapshotData = snapshot.pngData() else {
+            XCTFail("Failed to generate PNG data representation from snapshot", file: file, line: line)
+            return
+        }
+        
+        // ../SnapshotTestCITests/snapshots/[name].png
+        let snapshotURL = URL(fileURLWithPath: String(describing: file))
+            .deletingLastPathComponent()
+            .appendingPathComponent("snapshots")
+            .appendingPathComponent("\(name).png")
+        
+        
+        guard let storedSnapshotData = try? Data(contentsOf: snapshot) else {
+            XCTFail("Failed to load stored snapshot at URL: \(snapshotURL). Use `record` method to store a snapshot before asserting.", file: file, line: line)
+            return
+        }
+        
+        if snapshotData != storedSnapshotData {
+            let temporaryURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true).appendingPathComponent(snapshotURL.lastPathComponent)
+            
+            try? snapshotData.write(to: temporaryURL)
+            
+            XCTFail("New snapshot does not match sotred snapshot. New snapshot URL: \(temporaryURL), Stored snapshot URL: \(snapshotURL)", file: file, line: line)
+        }
         
         do {
             try FileManager.default.createDirectory(at: snapshotURL.deletingLastPathComponent(), withIntermediateDirectories: true)
