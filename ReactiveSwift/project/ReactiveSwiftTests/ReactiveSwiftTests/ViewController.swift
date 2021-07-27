609dff8d237ce49cedd509a540bc8c1420880602
Wilmer Barrios <40615926+wilbarrios@users.noreply.github.com>
2021-07-27 17:18:57 -0400

    ReactiveSwift primitive types

--- ReactiveSwift/project/ReactiveSwiftTests/ReactiveSwiftTests/ViewController.swift
+++ ReactiveSwift/project/ReactiveSwiftTests/ReactiveSwiftTests/ViewController.swift
@@ -7,12 +7,25 @@
 
 import UIKit
 import ReactiveSwiftTestsCoreiOS
+import ReactiveSwift
 
 class ViewController: UIViewController {
     
     private let textField: WBTextField = {
         WBTextField()
     }()
+    
+    private let button: UIButton = {
+        let b = UIButton()
+        b.setTitle("ACTION", for: .normal)
+        b.addTarget(self, action: #selector(buttonHandler), for: .touchUpInside)
+        return b
+    }()
+    
+    @objc
+    private func buttonHandler() {
+        print(#function)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,17 +49,29 @@ class ViewController: UIViewController {
         ])
     }
     
+    var disposable: Disposable?
+    
     private func setupObserver() {
         // Transform the signal
         let booleanSignal = textField.signal()
             .map({ $0.count > 0 }) // Has value?
         
         // Observe it
-        booleanSignal.observeValues({
-            print("\($0)")
+//        booleanSignal.observeValues({
+//            print("\($0)")
+//        })
+        
+        let b = self.button
+        let observer = Signal<Bool, Never>.Observer(value: {
+            value in
+            b.isEnabled = value
         })
+        
+        disposable = booleanSignal.observe(observer)
     }
-
-
+    
+    deinit {
+        disposable?.dispose()
+    }
 }
 
